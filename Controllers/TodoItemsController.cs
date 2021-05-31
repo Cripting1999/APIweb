1021e69303d4cd9a52a828584e73b665dbc52fa6
Cripting1999 <s17120252@alumnos.itsur.edu.mx>
2021-05-30 21:30:50 -0500

    Crear API Web FIn

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -105,5 +105,12 @@ public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
         {
             return _context.TodoItems.Any(e => e.Id == id);
         }
+        private static TodoItemDTO ItemToDTO(TodoItem todoItem) =>
+    new TodoItemDTO
+    {
+        Id = todoItem.Id,
+        Name = todoItem.Name,
+        IsComplete = todoItem.IsComplete
+    };
     }
 }
